6772855057a2a4e38b6b4d8e7e32f9cff52ac5b2
ethai <eugene.thai@telus.com>
2023-04-19 20:15:12 +0000

    setup kubectx and aliases

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ sudo apt update && sudo apt install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get install terraform=0.14.9
+go install github.com/google/go-containerregistry/cmd/gcrane@latest
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
