4124dd91b17f367f6932b38b3adebff21828ca0a
ethai <eugene.thai@telus.com>
2023-04-20 20:39:25 +0000

    remove gcloud from setup

--- setup.sh
+++ setup.sh
@@ -5,12 +5,6 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get install terraform=0.14.9
 go install github.com/google/go-containerregistry/cmd/gcrane@latest
-
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-
-curl https://sdk.cloud.google.com > install.sh
-
-
-bash install.sh --disable-prompts
