8f77cc21fe367bf606dd7aeadcc94597d0578ca2
ethai <eugene.thai@telus.com>
2023-04-20 20:43:08 +0000

    speed up configure process bypass apt update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y gnupg software-properties-common curl
+# sudo apt update && sudo apt install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get install terraform=0.14.9
